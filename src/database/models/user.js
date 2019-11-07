06e3fe9e09ca47627fb269c811325a9c225c7c6d
hezronkimutai <hezron.kimutai@andela.com>
2019-11-07 09:47:14 +0200

    ch(setup sequelize): setup sequelize - setup babel [Finishes #169258618]

---
+++ src/database/models/user.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {});
+  user.associate = function(models) {
+    // associations can be defined here
+  };
+  return user;
+};
